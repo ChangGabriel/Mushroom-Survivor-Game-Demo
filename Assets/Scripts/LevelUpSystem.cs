cbb4b4d50cb8b8d16fab8f68046f490b1ff4950b
Gabriel Chang <Gc.chang95@gmail.com>
2022-10-09 19:56:38 +0200

    Add levelup-system
    Added a basic levelup system for player.
    Player will now gain exp from killing enemies.
    All enemies have their own exp value.

---
+++ Assets/Scripts/LevelUpSystem.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelUpSystem : MonoBehaviour
+{
+    [SerializeField] private int level = 1;
+    public float currentExp { get; private set; } = 0;
+    private float requiredExp;
+    private PlayerAction playerAction;
+    private PlayerController playerController;
+
+    private void Start()
+    {
+        playerAction = GetComponent<PlayerAction>();
+        playerController = GetComponent<PlayerController>();
+    }
+
+    private int calculateRequiredExp(int currentLevel)
+    {
+        if (currentLevel == 0)
+        {
+            return 0;
+        }
+        else
+        {   //exp level formula
+            return (currentLevel + currentLevel * 5);
+        }
+    }
+
+    public void giveExp(float exp)
+    {
+        currentExp += exp;
+        requiredExp = calculateRequiredExp(level);
+
+        Debug.Log("Exp: " + currentExp + " / " + "Req: " + requiredExp);
+        if (currentExp >= requiredExp)
+        {
+            LevelUp();
+        }
+    }
+
+    private void LevelUp()
+    {
+        //Modify stats
+        level++;
+
+
+
+        //Modify Exp values
+        currentExp -= requiredExp;
+
+        Debug.Log("Level:" + level);
+
+    }
+
+
+
+}
